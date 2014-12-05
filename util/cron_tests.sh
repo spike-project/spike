6e84f0c858761306be65f732feb2c4879e1da8da
Marc-André Delsuc <madelsuc@unistra.fr>
2014-12-05 11:17:53 +0100

    cron_tests.sh correction

--- util/cron_tests.sh
+++ util/cron_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/local/bin/hg clone https://delsuc@bitbucket.org/NPK_v2/draft $HOME/tested_npkv2
+/usr/local/bin/hg clone https://delsuc@bitbucket.org/SPIKE $HOME/tested_npkv2
 cd $HOME/tested_npkv2
 $HOME/anaconda/bin/ipython Tests.py
 rm -R $HOME/tested_npkv2
