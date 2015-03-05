60ebfdeb0db1cc5cc194f8810e06cf037c9a69ab
Marc-André Delsuc <madelsuc@unistra.fr>
2015-03-05 19:02:17 +0100

    cron with ssh

--- util/cron_tests.sh
+++ util/cron_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/local/bin/hg clone https://delsuc@bitbucket.org/SPIKE $HOME/tested_npkv2
+/usr/local/bin/hg clone ssh://hg@bitbucket.org/delsuc/SPIKE $HOME/tested_npkv2
 cd $HOME/tested_npkv2
 $HOME/anaconda/bin/ipython Tests.py
 rm -R $HOME/tested_npkv2
