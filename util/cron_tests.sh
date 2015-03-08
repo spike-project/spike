18d96e2493ca86232bb663a0507197ae335761ad
MA.Delsuc <MA.Delsuc@localhost>
2015-03-08 18:02:46 +0100

    ...

--- util/cron_tests.sh
+++ util/cron_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 rm -fR $HOME/tested_spike_prev
 mv $HOME/tested_spike $HOME/tested_spike_prev
+mkdir $HOME/tested_spike
 cd $HOME/tested_spike
 /usr/local/bin/hg clone ssh://hg@bitbucket.org/delsuc/spike
 python -c "import spike.Tests as t; t.main()"
