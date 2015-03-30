054690d9afc8fcbe8b89a57ff8c36d31a8564706
Chiron <Chiron@localhost>
2015-03-30 11:33:04 +0200

    removed unused and unittests for mzXML

--- File/mzXML.py
+++ File/mzXML.py
@@ -2,11 +2,12 @@ import numpy as np
 import xml.dom.minidom
 import base64
 import struct
+import unittest
 
 
 class mzXML():
     '''
-    Draft for reqding mzXML files
+    Draft for reading mzXML files
     '''
     def __init__(self, filename):
         self.doc = xml.dom.minidom.parse(filename)
@@ -15,6 +16,8 @@ class mzXML():
     def txt2np(self, txt):
         '''
         from mzXML to numpy
+        Float 32, big endian.
+        Returns numpy format.
         '''
         data = base64.b64decode(txt)
         endian = '>'
@@ -26,6 +29,8 @@ class mzXML():
     def np2txt(self, nump):
         '''
         from numpy to mzXML
+        Float 32, big endian.
+        Returns text format.
         '''
         endian = '>'
         precision = 'f'
@@ -33,27 +38,56 @@ class mzXML():
         binary = struct.pack(endian + precision * count, *nump)
         txt = base64.b64encode(binary)
         return txt
-
+    
+    def scattr(self, scan, param, kind=None):
+        param_value = scan[0].getAttribute(param)
+        if kind:
+            return kind(param_value)
+        else:
+            return param_value
+    
     def get_params(self):
         '''
-        Extract parqmeters for limits and size
+        Extract parameters for limits and size
         '''
         spec = self.doc.getElementsByTagName('peaks')
         scan = self.doc.getElementsByTagName('scan')
+        
         num = self.txt2np(spec[0].childNodes[0].toxml())
-        print 'type(num) ', type(num)
         self.spec = num[0::2]
         self.mzaxis = num[1::2]
-        self.scan_size = len(self.txt2np(spec[0].childNodes[0].toxml()))/2
-        self.hmz = float(scan[0].getAttribute('highMz'))
-        self.lmz = float(scan[0].getAttribute('lowMz'))
+        self.scan_size = len(self.txt2np(spec[0].childNodes[0].toxml()))/2 # size of the scan
+        self.dic_param = {'msLevel':int, 'peaksCount':int, 'polarity':None, 'scanType':None,
+            'filterLine':None, 'retentionTime':None, 'lowMz':float, 'highMz':float,
+             'basePeakMz':float, 'basePeakIntensity':float, 'totIonCurrent':int, 'collisionEnergy':int}
+        for p in self.dic_param:
+            print "param ", p
+            try:
+                setattr(self, p, self.scattr(scan, p, self.dic_param[p])) 
+            except:
+                print "parameter {} not found".format(p)
         
     def save_mzXML(self, x, y, namefile_final):
         '''
-        Save the processed data in the new mzXML file.
+        Save data in the new mzXML file.
         '''
         spec = self.doc.getElementsByTagName('peaks')
         txt = self.np2txt(np.array(zip(x,y)).ravel())
         spec[0].childNodes[0].replaceWholeText(txt) # Replacing in the mzXML
         self.doc.writexml(open(namefile_final,'w')) # Saving the mzXML
         print 'mzXML saved'
+
+class mzXML_Tests(unittest.TestCase):  
+    def test_simple(self):
+        mzxml = mzXML('spike/File/testsmzXML/20131129_V_EV_bC_1499_CID30_r7k_25usc_range410-2000.mzXML')
+        dic_param = {'msLevel':2, 'peaksCount':35775, 'polarity':"+", 'scanType':"Full",
+            'filterLine':"FTMS + p NSI Full ms2 1499.60@cid30.00 [410.00-2000.00]",
+            'retentionTime':"PT0.0366S", 'lowMz':410.001, 'highMz':2069.29, 
+            'basePeakMz':1151.69, 'basePeakIntensity':13452.2, 'totIonCurrent':681064,
+            'collisionEnergy':30}
+        for p in dic_param:
+            getat = getattr(mzxml, p)
+            self.assertEqual(getattr(mzxml,p), dic_param[p])
+        
+if __name__ == '__main__':
+    unittest.main()
