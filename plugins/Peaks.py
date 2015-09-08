fb12378cf5bd6fe0e5cf4b9500650c04609e1c31
Marc-Andre' Delsuc <madelsuc@unistra.fr>
2015-09-09 00:11:37 +0200

    centroid1d and 2d work !

--- plugins/Peaks.py
+++ plugins/Peaks.py
@@ -16,6 +16,7 @@ import unittest
 from spike import NPKError
 from spike.NPKData import NPKData_plugin, NPKData
 from spike.util.counter import timeit
+from scipy.optimize import curve_fit
 #from spike.util.signal_tools import findnoiselevel, findnoiselevel_2D
 
 def _identity(x):
@@ -25,9 +26,9 @@ class Peak(object):
     """ a generic class to store peaks
     defines :
     Id          a unique integer
-    label       a string - str(Id) by default
     intens      The intensity (the height of the largest point)
     area        The area/volume of the peak
+    label       a string 
     intens_err  The uncertainty of the previous values
     area_err    ..
     
@@ -35,7 +36,7 @@ class Peak(object):
     def __init__(self, Id, label, intens):
         self.Id = Id
         self.label = label
-        self.intens = intens    # intensity
+        self.intens = float(intens)    # intensity
         self.area = 0.0         # area under the curve
         self.intens_err = 0.0      # uncertainty on intensity
         self.area_err = 0.0     # uncertainty on area
@@ -51,10 +52,21 @@ class Peak1D(Peak):
     """
     def __init__(self, Id, label, intens, pos):
         super(Peak1D, self).__init__(Id, label, intens)
-        self.pos = pos
+        self.pos = float(pos)
         self.pos_err = 0.0
         self.width = 0.0
         self.width_err = 0.0
+    def report(self, f=_identity):
+        """
+        return a string with the peak description
+        f is a function used to transform the coordinate
+        indentity function is default,
+        for instance you can use something like
+        peaks.report(f=d.axis1.itop)    to get ppm values on a NMR dataset
+        order is "id, label, position, intensity"
+        """
+        return  "%d, %s, %.5f, %.5f, %.2f" % \
+            (self.Id, self.label, f(self.pos), self.intens, self.width) 
 class Peak2D(Peak):
     """a class to store a single 2D peak
     defines in addition to Peak
@@ -71,28 +83,35 @@ class Peak2D(Peak):
         self.widthF2 = 0.0
         self.posF1_err = 0.0
         self.posF2_err = 0.0
+    def report(self, f1=_identity, f2=_identity):
+        """
+        print the peak list
+        f1, f2 are two functions used to transform respectively the coordinates in F1 and F2
+        indentity function is default,
+        for instance you can use something like
+        peaks.report(f1=s.axis1.itop, f2=s.axis2.itop)    to get ppm values on a NMR dataset
+        order is "id, label, posF1, posF2, intensity"
+        """
+        return "%d, %s, %.5f, %.5f, %.5f" % \
+                (self.Id, self.label, f1(self.posF1), f2(self.posF2), self.intens)
         
-class Peak1DList(object):
+class Peak1DList(list):
     """
     store a list of peaks
     contains the array version of the Peak1D object :
     self.pos is the numpy array of the position of all the peaks
     and self[k] is the kth Peak1D object of the list
     """
-    def __init__(self):
-        self.id = None
-        self.pos = None
-        self.intens = None
-    def len(self):
-        return len(self.id)
-    def __len__(self):
-        return len(self.id)
-    def __getitem__(self,i):
-        return Peak1D(Id = self.id[i],
-                label = self.label[i],
-                intens = self.intens[i], 
-                pos = self.pos[i])
-    def export(self, f=_identity):
+    @property
+    def pos(self):
+        return np.array( [pk.pos for pk in self] )
+    @property
+    def intens(self):
+        return np.array( [pk.intens for pk in self] )
+    @property
+    def label(self):
+        return [pk.label for pk in self]
+    def report(self, f=_identity):
         """
         print the peak list
         f is a function used to transform respectively the coordinates
@@ -102,32 +121,34 @@ class Peak1DList(object):
         """
         print ("# Peak list")
         print( "id, label, position, intensity")
-        for i in range(len(self)):
-            print("%d, %s, %.5f, %.5f" % \
-                (self.id[i], self.label[i], f(self.pos[i]), self.intens[i]) )
-class Peak2DList(object):
+        for pk in self:
+            print(pk.report(f=f))
+class Peak2DList(list):
     """
     store a list of peaks
     contains the array version of the Peak2D object :
     self.posF1 is the numpy array of the position of all the peaks
     and self[k] is the kth Peak2D object of the list
     """
-    def __init__(self):
-        self.id = None
-        self.posF1 = None
-        self.posF2 = None
-        self.intens = None
-    def len(self):
-        return len(self.id)
-    def __len__(self):
-        return len(self.id)
-    def __getitem__(self,i):
-        return Peak2D(Id = self.id[i],
-                label = self.label[i],
-                intens = self.intens[i], 
-                posF1 = self.posF1[i],
-                posF2 = self.posF2[i] )
-    def export(self, f1=_identity, f2=_identity):
+    @property
+    def posF1(self):
+        return np.array( [pk.posF1 for pk in self] )
+    @property
+    def posF2(self):
+        return np.array( [pk.posF2 for pk in self] )
+    @property
+    def intens(self):
+        return np.array( [pk.intens for pk in self] )
+    @property
+    def label(self):
+        return [pk.label for pk in self]
+    # def __getitem__(self,i):
+    #     return Peak2D(Id = self.id[i],
+    #             label = self.label[i],
+    #             intens = self.intens[i], 
+    #             posF1 = self.posF1[i],
+    #             posF2 = self.posF2[i] )
+    def report(self, f1=_identity, f2=_identity):
         """
         print the peak list
         f1, f2 are two functions used to transform respectively the coordinates in F1 and F2
@@ -137,9 +158,8 @@ class Peak2DList(object):
         """
         print ("# Peak list")
         print( "id, label, posF1, posF2, intensity")
-        for i in range(len(self)):
-            print("%d, %s, %.5f, %.5f, %.5f" % \
-                (self.id[i], self.label[i], f1(self.posF1[i]), f2(self.posF2[i]), self.intens[i]) )
+        for pk in self:
+            print(pk.report(f1=f1, f2=f2))
 #     npkd.peaks_ordered = npkd.peaks[np.argsort(a_buf[npkd.peaks])[::-1]]        # list from maximum to             
 def _peaks2d(npkd, threshold = 0.1, zoom = None, value = False, zones=0):
     '''
@@ -166,23 +186,18 @@ def peakpick(npkd, threshold = None, zoom = None):
         if threshold is None:
             threshold = 3*npkd.std()
         listpkF1, listint = peaks1d(npkd, threshold=threshold, zoom=zoom)
-        pp = Peak1DList()
-        pp.id = range(len(listpkF1))
-        pp.pos = listpkF1
-        pp.intens = listint
-        pp.label = ["%d"%i for i in pp.id]
-        npkd.peaks = pp
+                            #     Id, label, intens, pos        
+        pkl = Peak1DList( [ Peak1D(i, str(i), intens, pos) \
+            for i, pos, intens in zip( range(len(listint)), list(listpkF1), list(listint) ) ] )
+        npkd.peaks = pkl
     elif npkd.dim == 2:
         if threshold is None:
             threshold = 3*npkd.std()
         listpkF1, listpkF2, listint = peaks2d(npkd, threshold=threshold, zoom=zoom)
-        pp = Peak2DList()
-        pp.id = range(len(listpkF1))
-        pp.posF1 = listpkF1
-        pp.posF2 = listpkF2
-        pp.intens = listint
-        pp.label = ["%d"%i for i in pp.id]
-        npkd.peaks = pp
+                            #     Id, label, intens, posF1, posF2 
+        pkl = Peak2DList( [ Peak2D(i, str(i), intens, posF1, posF2) \
+            for i, posF1, posF2, intens in zip( range(len(listpkF1)), listpkF1, listpkF2, listint ) ] )
+        npkd.peaks = pkl
     else:
         raise NPKError("Not implemented of %sD experiment"%npkd.dim, data=npkd)
     return npkd
@@ -255,7 +270,25 @@ def peaks1d(npkd, threshold, zoom):
     listint = npkd.buffer[listpkF1]
     return listpkF1, listint
 #-------------------------------------------------------
-def centroid1d(npkd, npoints=3):
+# centroid measure
+def center(x, xo, intens, width):
+    """
+    the centroid definition, used to fit the spectrum
+    x can be a nparray
+    FWMH is   sqrt(2) x width.
+    """
+    return intens*(1 - ((x-xo)/width)**2)
+def center2d(yx, yo, xo, intens, widthy, widthx):
+    """
+    the 2D centroid, used to fit 2D spectra - si center()
+    xy is [x0, y_0, x_1, y_1, ..., x_n-1, y_n-1] - is 2*n long for n points,
+    returns [z_0, z_1, ... z_n-1]
+    """
+    y = yx[::2]
+    x = yx[1::2]
+    return intens*(1 - ((x-xo)/widthx)**2)*(1 - ((y-yo)/widthy)**2)
+## previous version - based on polyfit - different API
+def _centroid1d(npkd, npoints=3):
     """
     from peak lists determined with peak()
     realize a centroid fit of the peak summit and width,
@@ -285,6 +318,75 @@ def centroid1d(npkd, npoints=3):
         npkd.width_peaks.append(width)
     npkd.centered_peaks = np.array(npkd.centered_peaks)
     npkd.width_peaks = np.array(npkd.width_peaks)
+def centroid1d(npkd, npoints=3):
+    """
+    from peak lists determined with peak()
+    realize a centroid fit of the peak summit and width,
+    computes Full width at half maximum
+    updates in data peak list
+    
+    TODO : update uncertainties
+    """
+    from scipy import polyfit
+    npkd.check1D()
+    noff = (int(npoints)-1)/2
+    if (2*noff+1 != npoints) or (npoints<3):
+        raise NPKError("npoints must odd and >2 ",data=npkd)
+    for pk in npkd.peaks:
+        xdata = range(int(round(pk.pos-noff)), int(round(pk.pos+noff+1)))
+        ydata = npkd.get_buffer()[xdata]
+        try:
+            popt, pcov = curve_fit(center, xdata, ydata, p0=[pk.pos, pk.intens, 1.0] ) # fit
+        except RuntimeError:
+            print (pk.label, "centroid could not be fitted")
+        pk.pos = popt[0]
+        pk.intens = popt[1]
+        pk.width = popt[2]
+def centroid2d(npkd, npoints_F1=3, npoints_F2=3):
+    """
+    from peak lists determined with peak()
+    realize a centroid fit of the peak summit and width,
+    computes Full width at half maximum
+    updates in data peak list
+    
+    TODO : update uncertainties
+    """
+    from scipy import polyfit
+    npkd.check2D()
+    nF1 = npoints_F1
+    nF2 = npoints_F2
+    noff1 = (int(nF1)-1)/2
+    noff2 = (int(nF2)-1)/2
+    if (2*noff1+1 != nF1) or (nF1<3) or (2*noff2+1 != nF2) or (nF2<3):
+        raise NPKError("npoints must odd and >2 ",data=npkd)
+    for pk in npkd.peaks:
+        st1 = int(round(pk.posF1-noff1))
+        end1 = int(round(pk.posF1+noff1+1))
+        st2 = int(round(pk.posF2-noff2))
+        end2 = int(round(pk.posF2+noff2+1))
+        yxdata = np.array( [(y,x) for y in range(st1, end1) for x in range(st2, end2)] ).ravel()
+        # yx = np.array([[y,x] for y in range(1,4) for x in range(10,12)]).ravel()
+        # => array([ 1, 10,  1, 11,  2, 10,  2, 11,  3, 10,  3, 11])  will be decoded by center2d
+        zdata = npkd.get_buffer()[st1:end1, st2:end2].ravel()
+        try:
+                                            # yx,           yo, xo, intens, widthy, widthx
+            popt, pcov = curve_fit(center2d, yxdata, zdata, p0=[pk.posF1, pk.posF2, pk.intens, 1.0, 1.0] ) # fit
+        except RuntimeError:
+            print (pk.label, "centroid could not be fitted")
+        pk.posF1 = popt[0]
+        pk.posF2 = popt[1]
+        pk.intens = popt[2]
+        pk.widthF1 = popt[3]
+        pk.widthF2 = popt[4]
+#-------------------------------------------------------
+def centroid(npkd, *arg, **kwarg):
+    if npkd.dim == 1:
+        centroid1d(npkd, *arg, **kwarg)
+    elif npkd.dim == 2:
+        centroid2d(npkd, *arg, **kwarg)
+    else:
+        raise Exception("to be done")
+    return npkd
 #-------------------------------------------------------
 def display_peaks(npkd, axis = None, peak_label = False, zoom = None, show = False):
     if npkd.dim == 1:
@@ -293,7 +395,6 @@ def display_peaks(npkd, axis = None, peak_label = False, zoom = None, show = Fal
         return display_peaks2D(npkd, axis=axis, peak_label=peak_label, zoom=zoom, show=show)
     else:
         raise Exception("to be done")
-
 def display_peaks1D(npkd, axis = None, peak_label = False, zoom = None, show = False):
     """displays 1D peaks"""
     import spike.Display.testplot as testplot
@@ -307,7 +408,7 @@ def display_peaks1D(npkd, axis = None, peak_label = False, zoom = None, show = F
     else:
         z0 = 0
         z1 = npkd.size1
-        pk = xrange(pl.len())
+        pk = range(len(pl))
     if axis is None:
         plot.plot(pl.pos[pk], pl.intens[pk], "x")
         if peak_label:
@@ -337,12 +438,15 @@ def display_peaks2D(npkd, axis = None, peak_label = False, zoom = None, show = F
         z1up=npkd.size1-1
         z2lo=0
         z2up=npkd.size2-1
-        pk = xrange(pl.len())
+        pk = range(len(pl))
     if axis is None:
-        plot.plot(pl.posF2[pk], pl.posF1[pk], "x")
+        plF1 = pl.posF1 # these are ndarray !
+        plF2 = pl.posF2
+        plot.plot(plF2[pk], plF1[pk], "x")
         if peak_label:
             for p in pk:
-                plot.text(1.01*pl.posF2[p], 1.01*pl.posF1[p], pl.label[p])
+                plp = pl[p]
+                plot.text(1.01*plp.posF2, 1.01*plp.posF1, plp.label)
     else:
         raise Exception("to be done")
     if show: plot.show()
@@ -360,7 +464,6 @@ class PeakTests(unittest.TestCase):
         self.assertEqual(list(d.peaks.posF1) , [ 5, 10])
         self.assertEqual(list(d.peaks.posF2) , [ 7, 12])
         self.assertEqual(list(d.peaks.intens) , [ 20.0, 20.0])
-        
     def test_peaks1d(self):
         "test 1D peak picker"
         print(self.test_peaks1d.__doc__)
@@ -371,9 +474,42 @@ class PeakTests(unittest.TestCase):
         M[10] = 20
         d = NPKData(buffer = M)
         d.pp(threshold=3)
-        self.assertEqual(list(d.peaks.pos) , [ 5, 7, 10, 15])
+        self.assertEqual(list(d.peaks.pos) , [ 5.0, 7.0, 10.0, 15.0])
         self.assertEqual(list(d.peaks.intens) , [ 20.0, 8.0, 20.0, 11.0])
+    def test_center1d(self):
+        x = np.arange(5)
+        y = center(x, 2.2, 10.0, 1.2)
+        # y == [ -2.36111111e+01  -4.44089210e-15   9.72222222e+00   5.55555556e+00 -1.25000000e+01]
+        self.assertAlmostEqual(y[2], 9.72222222)
+        d = NPKData(buffer = np.maximum(y,0.0))
+        d.peaks = Peak1DList()
+        d.peaks.append(Peak1D(0, "0", 9.7, 2 ))
+        d.peaks[-1].width = 1.0
+        d.centroid(npoints=3)
+        self.assertAlmostEqual(d.peaks[0].pos,2.2)
+        self.assertAlmostEqual(d.peaks[0].intens,10.0)
+        self.assertAlmostEqual(d.peaks[0].width,1.2)
+    def test_center2d(self):
+        M=np.zeros((20, 20))
+        # add one peak at (F1,F2) 5.3, 7.9 with widthes (5.0,1.3) 
+        for y in range(1,10):
+            for x in range(6,11):
+                #                 yo, x0 intens, widthy, widthx
+                M[y,x] = center2d(np.array([y,x]), 5.3, 7.9, 20.0, 5.0, 1.3)
+        #print (M[1:10,6:11])
+        self.assertAlmostEqual(M[2,7],5.87777515)
+        d = NPKData(buffer = np.maximum(M,0.0))
+        d.peaks = Peak2DList()
+        # self, Id, label, intens, posF1, posF2 
+        d.peaks.append(Peak2D(0, "0", 18.0, 5, 8 ))
+        d.centroid(npoints_F1=5)
+        self.assertAlmostEqual(d.peaks[0].posF1,5.3)
+        self.assertAlmostEqual(d.peaks[0].posF2,7.9)
+        self.assertAlmostEqual(d.peaks[0].intens,20.0)
+        self.assertAlmostEqual(d.peaks[0].widthF1,5.0)
+        self.assertAlmostEqual(d.peaks[0].widthF2,1.3)
 
 NPKData_plugin("display_peaks", display_peaks)
 NPKData_plugin("pp", peakpick)
+NPKData_plugin("centroid", centroid)
 
