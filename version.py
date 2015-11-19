cbc0eff2d4882725f7a4252ee2698696177813d4
Marc-André Delsuc <madelsuc@unistra.fr>
2015-11-19 18:29:57 +0100

    version 0.7.0

--- version.py
+++ version.py
@@ -1,24 +1,18 @@
-#!/usr/bin/env python 
-# encoding: utf-8
-from __future__ import print_function
 
 # This file is generated automatically by dev_setup.py 
 # Do not edit
-
-
-
 ProgramName = 'SPIKE'
 VersionName = 'Development version - beta'
-version = '0.6.4'
-rev_date = '12-03-2015'
+version = '0.7.0'
+rev_date = '19-11-2015'
 
 def report():
     "prints version name when SPIKE starts"
-    print('''
+    print '''
     ========================
           %s
     ========================
     Version     : %s
     Date        : %s
-    ========================'''%(ProgramName, version, rev_date))
+    ========================'''%(ProgramName, version, rev_date)
 report()
