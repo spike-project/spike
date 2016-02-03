c6b442c8714e8d1576e7697078c667ec23bd226d
Marc-André Delsuc <madelsuc@unistra.fr>
2016-02-03 21:42:03 +0100

    still working on the documentation

--- util/cron_tests.sh
+++ util/cron_tests.sh
@@ -1,8 +1,47 @@
 #!/bin/sh
 # This can be used in a cron job to test spike regularly
+
+# adpt those to your own set-up
+# the commands
+HG=/usr/local/bin/hg
+PYTHON=/usr/local/anaconda/bin/python
+# your email
+EMAIL=youradress@mail.com
+#location of DATA files for tests
+DATAdir=/Volumes/DATA_test
+
+# then prepare directories
 rm -fR $HOME/tested_spike_prev
 mv $HOME/tested_spike $HOME/tested_spike_prev
 mkdir $HOME/tested_spike
 cd $HOME/tested_spike
-/usr/local/bin/hg clone ssh://hg@bitbucket.org/delsuc/spike
-python -m spike.Tests -m youradress@mail.com -D /Volumes/DATA_test 
+
+# clone code from repository
+$HG clone ssh://hg@bitbucket.org/delsuc/spike
+
+# and run tess
+$PYTHON -m spike.Tests -D $DATAdir -m madelsuc@unistra.fr -m lionel.chiron@gmail.com
+
+# Now build the documentation
+# this account must have read/write access to the repository for the doc be push backed
+# get the code
+$HG clone ssh://hg@bitbucket.org/spikedoc/spikedoc.bitbucket.org
+
+#build doc
+sh spike/doc/script_doc.sh 
+cd spikedoc.bitbucket.org
+
+# add new files to repository
+for i in $($HG stat | awk '/\?/ {print $2}'); do $HG add $i; done
+$HG commit -m 'automatic nightly build'
+$HG push
+cd ..
+
+# Eventually:
+# update to devel branch
+cd spike
+$HG pull && $HG update devel
+cd ..
+# and run tests here !
+$PYTHON -m spike.Tests -D $DATAdir -m madelsuc@unistra.fr -m lionel.chiron@gmail.com
+
