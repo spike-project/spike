fe06c9bb54e823906f6cfaea1ef55701cc0bc1f1
cherni Afef <cherniafef@hotmail.fr>
2016-02-08 18:12:31 +0100

    plugin starting with '_' do not load

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -40,7 +40,7 @@ def load():
     for pgfile in sorted( glob.glob( os.path.join(__path__[0],"*.py") ) ):
         b = os.path.basename(pgfile)    # code.py
         pgmod = os.path.splitext(b)[0]  # code
-        if not pgfile.endswith('__init__.py'):
+        if not pgfile.startswith('_'):
             loadone(pgmod, pgfile)
                 
 def loadone(pluginname, pgfile=None):
