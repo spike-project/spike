1ee5ab855018abe953c80dce32497d1cf6249c3a
MA Delsuc <madelsuc@unistra.fr>
2016-05-25 23:07:05 +0200

    modification in the zoom3D plugin : zoom def  and made it ok for NMR as well

--- plugins/zoom3D.py
+++ plugins/zoom3D.py
@@ -1,3 +1,11 @@
+"""
+Code to display 2D data-sets in interactive 3D, using Mayavi
+
+Original code from Alice Lynch - mai 2016
+
+adapted my M-A Delsuc
+"""
+from __future__ import print_function
 
 import numpy as np
 
@@ -11,32 +19,46 @@ except:
 from spike import NPKError
 from spike.NPKData import NPKData_plugin
 import spike.NPKData as npk
-import spike.FTICR
-import numpy as np
-def zoom3D(npkd, zoom, fontsize = 0.7, font = 'times', colormap = 'blue-red', showaxes = True):
-    #assumes zoom is in format displayed by visu2D
-    #x = axis2
-    #y = axis 1
-    #z = intensity
-    z1lo = int(npkd.axis1.mztoi(zoom[1]))
-    z1up = int(npkd.axis1.mztoi(zoom[3]))
-    z2lo = int(npkd.axis2.mztoi(zoom[0]))
-    z2up = int(npkd.axis2.mztoi(zoom[2]))
-    d2 = npkd.extract(z1lo, z1up, z2lo, z2up)
-    d3 = d2.get_buffer()
-    d4 = np.array(d3)
-    d5 = d4.transpose()
+
+def zoom3D(npkd, zoom, fontsize=0.7, font='times', colormap='blue-red', showaxes=True):
+    """
+    use the zoom region and display it in interactive 3D
+    
+    zoom : f1lo, f1up, f2lo, f2up - expressed in current unit
+
+    remark, use a small zoom region !
+
+    x = axis 2
+    y = axis 1
+    z = intensity
+    """
+    # z1lo = int(npkd.axis1.ctoi(zoom[0]))
+    # z1up = int(npkd.axis1.ctoi(zoom[1]))
+    # z2lo = int(npkd.axis2.ctoi(zoom[2]))
+    # z2up = int(npkd.axis2.ctoi(zoom[3]))
+    z1lo, z1up, z2lo, z2up = npk.parsezoom(npkd, zoom)
+    #print (z1lo, z1up, z2lo, z2up)
+    # d2 = npkd.extract(*zoom)  # extract modifies the data - 
+    # d3 = d2.get_buffer()
+    # d4 = np.array(d3)
+    # d5 = d4.transpose()
+    # get_buffer() loads everything in memory - so we'll do it line by line
+    d5 = np.zeros((z2up-z2lo+1, z1up-z1lo+1))  # tranposed matrix compared to npkd
+    for i in range(z2lo, z2up+1):
+        cc = npkd.col(i)              # going column wise is probably faster ...
+        d5[i-z2lo,:] = cc[z1lo:z1up+1]  # taking a slice out of a npkdata returns a np.array
     zmax = np.amax(d5)
+    zmin = np.amin(d5) # 0  - some data-set are negative
     xmin = zoom[2]
-    xmax = zoom[0]
-    ymin = zoom[3]
+    xmax = zoom[3]
+    ymin = zoom[0]
     ymax = zoom[1]
-    zmin = 0
     mlab.figure(bgcolor=(1., 1., 1.), fgcolor = (0., 0., 0.))
-    mlab.surf(d5, extent = [0, 1000, 0, 1000, 0, 1000], warp_scale = 'auto', colormap = colormap)
-    ax = mlab.axes(x_axis_visibility = showaxes, y_axis_visibility = showaxes, z_axis_visibility = showaxes, xlabel = 'm/z (h)', ylabel = 'm/z (v)', zlabel = 'Intensity', ranges = [xmin, xmax, ymin, ymax, zmin, zmax], nb_labels = 5)
+    mlab.surf(d5, extent = [0, 1000, 0, 1000, 0, 1000], warp_scale='auto', colormap=colormap)
+    ax = mlab.axes(x_axis_visibility=showaxes, y_axis_visibility=showaxes, z_axis_visibility=showaxes, xlabel="F2 "+npkd.axis2.currentunit, ylabel="F1 "+npkd.axis1.currentunit, zlabel='Intensity', ranges=[xmin, xmax, ymin, ymax, zmin, zmax], nb_labels=5)
     ax.label_text_property.font_family = font
     ax.title_text_property.font_family = font
     ax.axes.font_factor = fontsize
+
 if ok:
     NPKData_plugin("zoomwindow", zoom3D)
