07e4624409dc846a8b213a418a6703082d139a13
MA Delsuc <madelsuc@unistra.fr>
2016-05-31 23:42:15 +0200

    small corrections on baseline correction, restored original API.

--- Algo/BC.py
+++ Algo/BC.py
@@ -36,13 +36,13 @@ def fitpolyL1(x, y, degree=2, power=1, method="Powell"):
     res = minimize(pmin, coeff0, args=(x,y), method=method)
     return res.x
 
-def bcL1_init(y, degree=2, power=1, method="Powell"):
+def bcL1(y, degree=2, power=1, method="Powell"):
     "compute a baseline on y using fitpolyL1"
     x = np.arange(1.0*y.size)
     coeff = fitpolyL1(x, y, degree=degree, power=power, method=method)
     return poly(x,coeff)
 
-def bcL1(args): 
+def bcL1_paral(args): 
     '''
     compute a baseline on y using fitpolyL1 for the parallel case.
     '''
@@ -79,32 +79,32 @@ def baseline0(y, degree=2, power=1, method="Powell",
         Method for making the baseline serially chunk after chunk.
         '''
         for i in range(1,nchunk-1):
-            tbl = bcL1_init(y[i*lsize-cov:(i+1)*lsize+cov], degree=degree, power=power) # Estimate
+            tbl = bcL1(y[i*lsize-cov:(i+1)*lsize+cov], degree=degree, power=power) # Estimate
             bl[i*lsize-cov:i*lsize+cov] = bl[i*lsize-cov:i*lsize+cov]*corrm1 + tbl[:2*cov]*corr # correction from 0 to 2*cov
             bl[i*lsize+cov:(i+1)*lsize+cov] = tbl[2*cov:] # rest of the baseline is the estimate
 
     nchunk = y.size/chunksize
     if nchunk <2:
-        bl = bcL1_init(y, degree=degree, power=power, method=method)
+        bl = bcL1(y, degree=degree, power=power, method=method)
     else:
         lsize = y.size/nchunk
         cov = int(lsize*ratiocov)           # covering parts
         corr = np.linspace(0.0,1.0,2*cov)   # simple weighting coeeficient for fusionning chunks. 
         corrm1 = 1.0-corr
         bl = np.zeros_like(y)
-        bl[0:lsize+cov] = bcL1_init(y[0:lsize+cov], degree=degree, power=power)
+        bl[0:lsize+cov] = bcL1(y[0:lsize+cov], degree=degree, power=power)
         i = 0 # if nchunk == 2 !
         ###
         if nbcores:             # Parallelization
             p = mp.Pool(nbcores)        # Multiprocessing Pool
             args = iter([[y[i*lsize-cov:(i+1)*lsize+cov], degree, power, method] for i in range(1,nchunk-1)])
-            res = p.imap(bcL1, args)
+            res = p.imap(bcL1_paral, args)
             approx_BL_parallel()    # Make the baseline in parallel
             p.close() 
         else:
             approx_BL_serial()      # Make the baseline in serial
         i = nchunk-1
-        tbl = bcL1_init(y[i*lsize-cov:-1], degree=degree, power=power)
+        tbl = bcL1(y[i*lsize-cov:-1], degree=degree, power=power)
         bl[i*lsize-cov:i*lsize+cov] = bl[i*lsize-cov:i*lsize+cov]*corrm1 + tbl[:2*cov]*corr # correction from 0 to 2*cov
         bl[i*lsize+cov:] = tbl[2*cov-1:] # rest of the baseline is the estimate ## -1
     return bl
@@ -142,9 +142,9 @@ def baseline1(y, degree=2, chunksize=2000):
 
 def correctbaseline(y, iterations=1, nbchunks = 100, firstpower=0.3,
                         secondpower=7, degree=1,  chunkratio=1.0,
-                        interv_ignore = None, method="Powell",
-                        nbcores= None,
-                        debug = False, choiceBL = 0, ratiocov=0.7):
+                        interv_ignore=None, method="Powell",
+                        nbcores=None,
+                        debug=False, choiceBL=0, ratiocov=0.7):
     '''
     Find baseline by using low norm value and then high norm value to attract the baseline on the small values.
     Parameters : 
@@ -163,6 +163,10 @@ def correctbaseline(y, iterations=1, nbchunks = 100, firstpower=0.3,
     '''
     if choiceBL == 0:
         baseline = baseline0
+    elif choiceBL == 1:
+        baseline = baseline1
+    else:
+        raise Exception("error with choiceBL")
     if interv_ignore:
         ii = interv_ignore
         delta = ii[1]-ii[0]
@@ -194,7 +198,14 @@ class BC_Tests(unittest.TestCase):
         b = baseline0(y,chunksize=N/20)
         corr = y-b
         self.assertTrue(np.std(corr) < 0.21)
-    def test_correctbaseline(self):
+    def test_baseline1(self):
+        N = 100000
+        x = np.linspace(0,10,N)
+        y = np.sin(x/2) + 0.2*np.random.randn(N)
+        b = baseline1(y,chunksize=N/20)
+        corr = y-b
+        self.assertTrue(np.std(corr) < 0.21)
+    def _test_correctbaseline(self):
         N = 100000
         x = np.linspace(0,10,N)
         y = np.sin(x/2) + 0.2*np.random.randn(N)
