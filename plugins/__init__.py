6022b787227bb8562330509ac3d7f3e2f51add44
MA Delsuc <madelsuc@unistra.fr>
2017-06-01 23:01:10 +0200

    improved plugin loading and documentation

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -29,10 +29,12 @@ from __future__ import print_function #
 import imp
 import traceback
 import unittest
-
-plugins = []  # contains plugin modules loaded so far
+from collections import defaultdict
 import glob, os
-from ..NPKData import NPKData_plugin
+#from ..NPKData import NPKData_plugin
+
+plugins = {}  # contains plugin modules loaded so far
+codes = defaultdict(list)  # loaded at plugin injection with all codes
 
 def load(debug=True):
     "the load() function is called at initialization, and loads all files found in the plugins folder"
@@ -42,7 +44,31 @@ def load(debug=True):
         pgmod = os.path.splitext(b)[0]  # code
         if not b.startswith('_'):
             loadone(pgmod, pgfile, debug=debug)
-                
+    print( "plugins loaded:\n" + 
+           " ".join(["{}, ".format(k) for k in plugins.keys()]) )
+    print( "type spike.plugins.report() for a short description")
+
+def report(module=None, mode="short"):
+    """ print a nice report
+    mode=choose the verbosity
+        "short": only one line (default) 
+        "detailed": with list of all added methods
+        "full": prints the whole plugin doc
+    """
+    if module is None:
+        todo = sorted(plugins.keys())
+    else:
+        todo = [module]
+    for k in todo:
+        doc = plugins[k]
+        if mode != "full":
+            doc = doc.split('\n')[0]  # only first line
+        else:
+            print("="*60)
+        print("%s : %s"%(k, doc))
+        if  mode != "short":
+            print("    implements: "+" ".join(["{}, ".format(c) for c in codes[k]]))
+
 def loadone(pluginname, pgfile=None, debug=True):
     """
     loads the plugin "pluginname" found in file pgfile
@@ -54,14 +80,15 @@ def loadone(pluginname, pgfile=None, debug=True):
         pgfile = os.path.join(direc, pluginname + ".py")
     if debug: print("---- Importing  << %s >>"%pluginname)
     try:
-        plugins.remove(pluginname)
+        plugins[pluginname]
         if debug: print("WARNING existing plugin %s is overwritten"%pluginname)
-    except    ValueError:
+    except    KeyError:
         pass
     try:
         m = imp.load_source(pluginname, pgfile)
-        if debug: print("     "+m.__doc__.split('\n')[0])    # print first line of documentation
-        plugins.append(pluginname)
+        doc = m.__doc__.split('\n')[0]    # print first line of documentation
+        if debug: print("     "+doc)
+        plugins[pluginname] = m.__doc__
     except:
         print("*** Importing  << %s >> Failed ***"%pluginname)
         traceback.print_exc()
