9cbec69d0a76472a5b0987136cf4400b49de805e
MA Delsuc <madelsuc@unistra.fr>
2017-10-04 10:00:01 +0200

    added SANE and PALMA algo, Spinit format, INTER tools; update of extract(),

---
+++ File/Spinit.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python 
+# encoding: utf-8
+
+"""
+    Utility to Handle NMR Spinit files
+
+"""
+
+from __future__ import print_function
+
+__author__ = "Marc Andre' Delsuc"
+__date__ = "August 2017"
+
+import os
+import os.path as op
+import glob
+import re
+import struct
+import time
+import unittest
+import shutil
+import xml.etree.ElementTree as ET
+
+
+import numpy as np
+
+from ..NPKData import NPKData
+
+debug = False
+verbose = False   # change this for verbose importers
+
+################################################################
+def load_header(filename="header.xml"):
+    """ 
+    loads a header.xml as an ET.tree, and keep it in memory
+
+    returns headertree
+    xml parameters are found in headertree.getroot()
+    
+    """
+    tree = ET.parse(filename)
+    return tree
+
+def read_param(filename="header.xml"):
+    """ 
+    loads a header.xml as a dictionnary key:value
+    
+    """
+    debug = 0
+    headertree = load_header(filename)
+    xroot = headertree.getroot()
+    xparams = xroot[0]
+    params = {}
+    for entry in xparams:
+        k = entry.find('key').text
+        params[k] = ""
+        for xv in entry.iter('value'):
+            try:
+                params[k] += xv.text.strip()
+            except AttributeError:
+                pass
+# debug code
+    if debug:
+        for i in dict.keys():
+            print(i+" = "+str(dict[i]))
+    return params
+
+def modify_val(headertree, key, value):
+    """
+    modify an entry from a headertree loaded with load_header()
+    key should be present in headertree
+    key and val are both strings
+
+    headertree can then written back to disk with
+        headertree.write('header.xml')
+    """
+    debug = 1
+    for entry in headertree.getroot()[0]:
+        k = entry.find('key').text
+        if k == key:                  # match
+            vals = entry.find('value')
+            bef = vals.find('value').text
+            vals.find('value').text = value
+            if debug:
+                print (key, 'Before:', bef, 'After:', vals.find('value').text)
+
+################################################################
+def read_1D(size, filename="data.dat"):
+    """
+    Reads in a Spinit 1D fid as a numpy float array
+    
+    size is the number of data-points in the fid
+    uses struct
+    does not check endianess
+    """
+    fmt = ">%df"
+    si = size
+    rl = np.empty((si))
+    im = np.empty((si))
+    with open(filename,"rb") as F:
+        buf = F.read(8*si)
+    ibuf = struct.unpack(fmt%(2*si), buf)  # upack the string as pairs of float
+    rl[:] = ibuf[::2]
+    im[:] = ibuf[1::2]
+    npkbuf = rl + 1j*im
+    return npkbuf
+    
+################################################################
+def read_2D(sizeF1, sizeF2, filename="data.dat",):
+    return npkbuf
+
+################################################################
+def zerotime(acqu):
+    """get digital filter parameters, if any
+    """
+    try:
+        zerotimeposition = float( acqu['DIGITAL_FILTER_SHIFT'] )
+    except:
+        zerotimeposition = 0.0
+    return(zerotimeposition)
+################################################################
+def offset(acqu, proc):
+    """
+    computes the offset from spinit to spike
+    """
+    try:
+        ppmPointUn = float(proc['$OFFSET'])
+        ppmWidth = float(acqu['$SW_h']) / float(acqu['$SFO1'])
+        calibrationOffset = float(ppmPointUn - ppmWidth)*  float(acqu['$SFO1'])
+    except:
+        calibrationOffset=0.0
+    return (calibrationOffset)
+################################################################
+def Import_1D(filename="data.dat"):
+    """
+    Imports a 1D spinit fid as a NPKData
+    
+    """
+    if (not op.exists(filename)):
+        raise Exception(filename+" : file not found")
+    dire=op.dirname(filename)
+    acqu = read_param(op.join(dire,'header.xml'))
+    size= int(acqu['ACQUISITION_MATRIX_DIMENSION_1D'])  # get size
+    if verbose: print("importing 1D FID, size =",size)
+    data = read_1D(size, filename)
+    d = NPKData(buffer=data)
+#    d.revf()
+# then set parameters
+    d.axis1.specwidth = float(acqu['SPECTRAL_WIDTH'])           # in Hz
+    d.axis1.frequency = float(acqu['OBSERVED_FREQUENCY'])/1E6   # in MHz
+    d.axis1.itype = 1      # always complex
+    d.axis1.offset = float(acqu['SR']) - d.axis1.specwidth/2
+
+    d.axis1.zerotime = zerotime(acqu)
+    pardic = {"header": acqu} # create ad-hoc parameters
+    d.params = pardic   # add the parameters to the data-set
+    return d
+
+################################################################
+def Export_1D(d, filename="data.dat", template="header.xml"):
+    """
+    export a 1D NPKData as a spinit
+    """
+    if d.dim >1:
+        raise Exception("not implemented yet for Dim>1")
+    if d.itype == 1:     # complex
+        array = d.get_buffer()
+    else:                   # real
+        array = d.get_buffer() + 0.0j   # add a zero imaginary part
+
+    dirname = op.dirname(filename)
+    if not op.isdir(dirname):
+        os.makedirs(dirname)
+    array.astype(">f").tofile(filename)
+
+# then set parameters
+    headertree = load_header(template)
+    modify_val(headertree, 'MODALITY', "NMR" )
+    modify_val(headertree, 'ACCU_DIM', "1" )
+    modify_val(headertree, 'ACQUISITION_MATRIX_DIMENSION_1D', str(d.axis1.cpxsize) )
+    modify_val(headertree, 'SPECTRAL_WIDTH', str(d.axis1.specwidth) )
+    modify_val(headertree, 'OBSERVED_FREQUENCY', str(d.axis1.frequency*1E6) )
+    modify_val(headertree, 'SR', str(d.axis1.offset+d.axis1.specwidth/2) )
+    print ('    **SR is wrong ! to-be-changed**')
+    modify_val(headertree, 'DIGITAL_FILTER_SHIFT', str(d.axis1.zerotime) )
+    headerfile = op.join( dirname, "header.xml")
+    headertree.write(headerfile)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ plugins/sane.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python 
+# encoding: utf-8
+
+"""plugin for Sane denoising
+
+This plugin implements the SANE denoising algorithm, 
+SANE is inspired from urQRd algorithm, but is improved in several points
+
+- faster on vector length != 2**n
+- much more efficient on weak signals
+- requires less iterations and less overestimate of rank
+- however, a non productive iteration is always performed,
+  so processing time for I iterations of SANE should be compared
+  with I+1 iterations of urQRd.
+
+associated publications
+- Bray, F., Bouclon, J., Chiron, L., Witt, M., Delsuc, M.-A., & Rolando, C. (2017).
+  Nonuniform Sampling Acquisition of Two-Dimensional Fourier Transform Ion Cyclotron Resonance Mass Spectrometry for Increased Mass Resolution of Tandem Mass Spectrometry Precursor Ions.
+  Analytical Chemistry, acs.analchem.7b01850. http://doi.org/10.1021/acs.analchem.7b01850
+
+- Chiron, L., van Agthoven, M. A., Kieffer, B., Rolando, C., & Delsuc, M.-A. (2014).
+  Efficient denoising algorithms for large experimental datasets and their applications in Fourier transform ion cyclotron resonance mass spectrometry.
+  PNAS , 111(4), 1385–1390. http://doi.org/10.1073/pnas.1306700111
+"""
+
+from __future__ import print_function
+
+import unittest
+import numpy as np
+from numpy.fft import fft, ifft, rfft, irfft
+
+from spike.NPKData import NPKData_plugin,  as_cpx, as_float, _base_fft,\
+            _base_ifft, _base_rfft, _base_irfft
+from spike.Algo.sane import sane
+from spike.util.signal_tools import filtering
+
+import sys #
+if sys.version_info[0] < 3:
+    pass
+else:
+    xrange = range
+
+def sane_plugin(npkd, rank, orda=None, iterations=1, axis=0, trick=True, optk=False, ktrick=False):
+    """
+    Apply "sane" denoising to data
+    rank is about 2 x number_of_expected_lines
+    Manages real and complex cases.
+    Handles the case of hypercomplex for denoising of 2D FTICR for example.
+    
+    sane algorithm. Name stands for Support Selection for Noise Elimination.
+    From a data series return a denoised series denoised
+    data : the series to be denoised - a (normally complex) numpy buffer
+    rank : the rank of the analysis
+    orda : is the order of the analysis
+        internally, a Hankel matrix (M,N) is constructed, with M = orda and N = len(data)-orda+1
+        if None (default) orda = (len(data)+1)/2
+    iterations : the number of time the operation should be repeated
+    optk : if set to True will calculate the rank giving the best recovery for an automatic estimated noise level. 
+    trick : permits to enhanced the denoising by using a cleaned signal as the projective space. "Support Selection"
+    ktrick : if a value is given, it permits to change the rank on the second pass.
+             The idea is that for the first pass a rank large enough as to be used to compensate for the noise while
+             for the second pass a lower rank can be used. 
+    
+    """
+    if npkd.dim == 1:
+        if npkd.axis1.itype == 0:   # real
+            buff = as_cpx(_base_ifft(_base_rfft(npkd.buffer)))       # real case, go to analytical signal
+        else:   #complex
+            buff = npkd.get_buffer()                       # complex case, makes complex
+        sane_result = sane( buff, rank, orda = orda, trick = trick, iterations = iterations) # performs denoising
+        if npkd.axis1.itype == 0:   # real
+            buff = _base_irfft(_base_fft(as_float(sane_result)))      # real case, comes back to real
+            npkd.set_buffer(buff)
+        else:
+            npkd.buffer = as_float(sane_result)             # complex case, makes real
+    elif npkd.dim == 2:
+         todo = npkd.test_axis(axis)
+         if todo == 2:
+             for i in xrange(npkd.size1):
+                 r = npkd.row(i).sane(rank=rank, orda=orda, iterations=iterations)
+                 npkd.set_row(i,r)
+         elif todo == 1:
+             for i in xrange(npkd.size2):
+                 r = npkd.col(i).sane(rank=rank, orda=orda, iterations=iterations)
+                 npkd.set_col(i,r)
+    elif npkd.dim == 3:
+         raise Exception("not implemented yet")
+    return npkd
+
+
+
+NPKData_plugin("sane", sane_plugin)
