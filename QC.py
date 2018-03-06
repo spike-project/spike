566c2d1a8291abdeb05ee9339fc45e7ae1811d05
Marc-André Delsuc <madelsuc@unistra.fr>
2018-03-06 12:44:09 +0100

    improved QC

--- QC.py
+++ QC.py
@@ -89,7 +89,6 @@ def find_depedencies(reader):
 
 def run_pylint(fich):
     "run pylint on the given file and return synthetic results (note, error, reorder, warning)"
-    import re
     from pylint import lint
     from pylint.reporters.text import TextReporter
 #    pyl = lint.Run(args)
@@ -100,7 +99,7 @@ def run_pylint(fich):
     errors = 0
     warn = 0
     reorder = 0
-    note = 0
+    note = 0.0
     for l in pylint_output:
         if l.startswith('E0001'):
             errors = 10000  # indicates crash
@@ -114,7 +113,7 @@ def run_pylint(fich):
         elif l.startswith('R') and l.split()[0] != 'Raw':
             reorder += 1
         elif l.startswith('Your code has been rated'):
-            m = re.match('Your code has been rated at (-?\d+.\d.)/10', l)
+            m = re.match(r'Your code has been rated at (-?\d+.\d.)/10', l)
             note = float(m.group(1))
         # elif l.find('External dependencies') !=-1:
         #     print "##############"
