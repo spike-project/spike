a2df3889e1cbf5376028af4b3322f05ab28eb7f5
Marc-André Delsuc <madelsuc@unistra.fr>
2019-03-08 10:28:05 +0100

    mini corrections in Tests

--- spike/Tests.py
+++ spike/Tests.py
@@ -51,7 +51,7 @@ RUN = True
 mod_util = ("plugins", 'util.dynsubplot', 'util.debug_tools')  #'util.read_msh5', 
 mod_algo = ('Algo.Cadzow', 'Algo.Linpredic', 'Algo.urQRd', 'Algo.sane', 'Algo.SL0', 'Algo.maxent', 'Algo.BC') 
 mod_plugins = ("plugins.Peaks", "plugins.Fitter", "plugins.Bruker_NMR_FT", "plugins.Peaks")
-mod_file = ("File.BrukerNMR", "File.GifaFile", 'File.HDF5File', 'File.Apex', 'File.csv', 'File.Solarix', 'File.mzXML')
+mod_file = ("File.BrukerNMR", "File.GifaFile", 'File.HDF5File', 'File.Apex', 'File.csv', 'File.Solarix')# , 'File.mzXML') - not py3 ready yet !
 mod_basicproc = ("NPKData", "FTICR", "Orbitrap", 'NPKConfigParser')
 mod_user = ('processing', )
 
