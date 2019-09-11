b08df0924bdc834f246a053b339f012dd6e59110
Marc-André Delsuc <madelsuc@unistra.fr>
2019-09-11 07:54:14 +0200

    release 0.99.12

--- spike/version_rev.py
+++ spike/version_rev.py
@@ -10,8 +10,8 @@ except:
     revision = "--not determined--"
 ProgramName = 'SPIKE'
 VersionName = 'Development version'
-version = '0.99.11'
-rev_date = '10-09-2019'
+version = '0.99.12'
+rev_date = '11-09-2019'
 
 def report():
     "prints version name when SPIKE starts"
